365271476b2d59e8c8ac289fa8b83e36319bc5d9
Vu-Hoang <hoangvubrvt@gmail.com>
2023-04-08 21:30:59 +0200

    init

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,18 +1,18 @@
-import fs from "fs";
+import fs from 'fs'
 
 export abstract class CsvFileReader<T> {
-    data: T[]= [];
+  data: T[] = []
 
-    abstract mapRow(row:string[]): T;
+  abstract mapRow (row: string[]): T
 
-    protected constructor(protected fileName: string) {}
+  protected constructor (protected fileName: string) {}
 
-    read() : void {
-        this.data = fs.readFileSync(this.fileName, {
-            encoding: 'utf-8'
-        })
-            .split('\n')
-            .map(row => row.split(','))
-            .map((row: string[]): any => this.mapRow(row));
-    }
+  read (): void {
+    this.data = fs.readFileSync(this.fileName, {
+      encoding: 'utf-8'
+    })
+      .split('\n')
+      .map(row => row.split(','))
+      .map((row: string[]): any => this.mapRow(row))
+  }
 }
--- src/Summary.ts
+++ src/Summary.ts
@@ -1,42 +1,38 @@
-import {MatchData} from "./MatchData";
-import {WinAnalysis} from "./analyzers/WinAnalyzer";
-import {HtmlReport} from "./reports/HtmlReport";
-import {ConsoleReport} from "./reports/ConsoleReport";
+import { type MatchData } from './MatchData'
+import { WinAnalysis } from './analyzers/WinAnalyzer'
+import { HtmlReport } from './reports/HtmlReport'
+import { ConsoleReport } from './reports/ConsoleReport'
 
 export interface Analyzer {
-    run(matches: MatchData[]): string;
+  run: (matches: MatchData[]) => string
 }
 
 export interface OutputTarget {
-    print(report: string): void
+  print: (report: string) => void
 }
 
 export class Summary {
+  static winsAnalysisWithHtmlReport (teamName: string): Summary {
+    return new Summary(
+      new WinAnalysis(teamName),
+      new HtmlReport()
+    )
+  }
 
-    static winsAnalysisWithHtmlReport(teamName: string): Summary {
-        return new Summary(
-            new WinAnalysis(teamName),
-            new HtmlReport()
-        )
-    }
+  static winsAnalysisWithConsoleReport (teamName: string): Summary {
+    return new Summary(
+      new WinAnalysis(teamName),
+      new ConsoleReport()
+    )
+  }
 
-    static winsAnalysisWithConsoleReport(teamName: string): Summary {
-        return new Summary(
-            new WinAnalysis(teamName),
-            new ConsoleReport()
-        )
-    }
+  constructor (
+    public analyzer: Analyzer,
+    public outputTarget: OutputTarget
+  ) {}
 
-    constructor (
-        public analyzer: Analyzer,
-        public outputTarget: OutputTarget
-    ) {}
-
-    buildAndPrintReport(matchData: MatchData[]) {
-        const data = this.analyzer.run(matchData);
-        this.outputTarget.print(data);
-    }
+  buildAndPrintReport (matchData: MatchData[]): void {
+    const data = this.analyzer.run(matchData)
+    this.outputTarget.print(data)
+  }
 }
-
-
-
